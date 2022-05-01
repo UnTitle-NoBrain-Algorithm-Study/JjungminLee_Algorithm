58cd1448727d32d901ba334d01463037924e72a3
Jungmin Lee <julie0964@naver.com>
2022-05-01 16:29:39 +0900

    알고리즘 스터디 5주차 LV5.알약

--- week5/LV5.java
+++ week5/LV5.java
@@ -5,35 +5,50 @@ import static java.lang.Math.max;
 
 public class Main {
     public static long cnt=0;
-    public static long dp[];
+    public static long dp[][];
     public static int n;
+
+    //dp[한알 개수][반알 개수]
+
+    public static long medicine(int one,int half){
+        if (one==0){
+            return 1;
+        }
+
+        //실질적으로 리턴
+        if(dp[one][half]!=0){
+            return dp[one][half];
+        }
+
+        long cnt=0;
+
+        if(one!=0){
+            cnt+=medicine(one-1,half+1);
+        }
+        if(half!=0){
+            cnt+=medicine(one,half-1);
+        }
+
+        return dp[one][half]=cnt;
+    }
+
     public static void main(String[]args) throws IOException {
 
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        dp=new long[31];
-        dp[0]=1;
-        dp[1]=1;
-        dp[2]=2;
+        dp=new long[31][31];
 
+        dp[1][0]=1;
+        dp[2][0]=2;
+        dp[3][0]=5;
+        medicine(30,0);
         while(true){
             n=Integer.parseInt(br.readLine());
-            if(n==0) {
+            if(n==0){
                 break;
-            }
-            else{
-               
-                for(int i=3;i<=30;i++) {
-                    cnt=0;//초기화
-                    for (int j = 0; j < i; j++) {
-                        cnt += dp[j] * dp[i - 1 - j];
-                    }
-                    dp[i] = cnt;
-                }
-
-                System.out.println(dp[n]);
+            }else{
+                System.out.println(dp[n][0]);
             }
         }
-
 
 
     }
