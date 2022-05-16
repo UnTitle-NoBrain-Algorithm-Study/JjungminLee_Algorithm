ca13f123aca15720465333b2a2e535373754c62d
JjungminLee <julie0964@naver.com>
2022-05-17 06:40:40 +0900

    알고리즘 스터디 8주차 알고리즘 수업 - 너비 우선 탐색 1

---
+++ week8/LV1.java
@@ -0,0 +1,91 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static int n,m,r;
+    public static List<Integer>[]arr;
+    public static boolean []visited;
+    public static int []order;
+    public static int cnt=0;
+
+    public static void BFS(int n){
+        Queue<Integer>queue=new LinkedList<Integer>();
+        queue.add(n);
+        visited[n]=true;
+
+
+
+
+        while(!queue.isEmpty()){
+
+            int x=queue.poll();
+            cnt++;
+            order[x]=cnt;
+
+            for(int node:arr[x]){
+                if(!visited[node]){
+
+                    queue.add(node);
+                    visited[node]=true;
+
+                }
+            }
+
+
+        }
+
+
+    }
+
+
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        m=sc.nextInt();
+        r=sc.nextInt();
+        arr=new ArrayList[n+1];
+        visited=new boolean[n+1];
+        order=new int[n+1];
+        Arrays.fill(order,0);
+        for(int i=0;i<=n;i++){
+            arr[i]=new ArrayList<Integer>();
+        }
+
+        for(int i=0;i<m;i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            arr[a].add(b);
+            arr[b].add(a);
+        }
+        for(int i=1;i<=n;i++){
+            Collections.sort(arr[i]);
+
+        }
+        BFS(r);
+
+
+        for(int i=1;i<=n;i++){
+            System.out.println(order[i]);
+        }
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
